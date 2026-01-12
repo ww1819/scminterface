b177f76d207c18bc80f3adb26100f787e3731762
ww1819 <1737914508@qq.com>
2026-01-12 14:49:45 +0800

    feat(controller): 添加衡水三数据控制器
    新增 HengshuisanController 用于处理衡水三相关接口请求

---
+++ scminterface-framework/src/main/java/com/scminterface/framework/web/controller/HengshuisanController.java
@@ -0,0 +1,22 @@
+package com.scminterface.framework.web.controller;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import io.swagger.annotations.Api;
+
+/**
+ * 衡水三数据控制器
+ * 
+ * @author scminterface
+ */
+@Api(tags = "衡水三数据")
+@RestController
+@RequestMapping("/api/hengshui")
+public class HengshuisanController
+{
+    private static final Logger log = LoggerFactory.getLogger(HengshuisanController.class);
+
+    // 方法待实现
+}
